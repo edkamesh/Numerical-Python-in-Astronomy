63f835e0d738b6c51146854e47f55fd04135e679
Kamesh R <122994495+edkamesh@users.noreply.github.com>
2023-02-01 10:04:07 +0530

    Update decilination.py

--- decilination.py
+++ decilination.py
@@ -1,2 +1,12 @@
-
+import math
+x=int(input("Enter the value of N: "))
+i=1
+theta=360/365.24
+e_not=math.radians(23.44)
+print("Day\t Declination\n")
+for i in range(0,364):
+    if i<x:
+        i=i+1
+        d=(-math.asin(math.sin(e_not)*math.cos(theta*(i+10))))
+        print(i,"\t""{:.3f}".format(d))
  
